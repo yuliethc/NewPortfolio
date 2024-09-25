5f9218711718cedc96d922b61b727735f38ddf35
Yulieth Lubo <yuliethdotcom@gmail.com>
2024-09-25 09:35:51 +0200

    commit img and text in gasby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Yulieth Lubo',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+      'Yulieth Lubo is a software developer specializing in building (and occasionally designing) exceptional digital experiences.',
+    siteUrl: 'https://yulieth.com', 
+    image: '/og.png',
+    twitterUsername: '@luboyulieth',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Yulieth Lubo',
+        short_name: 'Yulieth Lubo',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
