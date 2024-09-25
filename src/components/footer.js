16c499b4836700c2436ef5517e938e8f3da73493
Yulieth Lubo <yuliethdotcom@gmail.com>
2024-09-25 09:46:15 +0200

    names

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,21 +105,8 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
-
-          {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )}
+        <a href="https://github.com/yuliethc/NewPortfolio">
+          <div>Copyright (c) 2024 Yulieth Lubo</div>
         </a>
       </StyledCredit>
     </StyledFooter>
