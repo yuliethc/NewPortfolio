98b6c4547d1e15ea8f97d1b1a92120982681b151
Yulieth Lubo <yuliethdotcom@gmail.com>
2024-09-25 09:59:19 +0200

    color var pink

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -14,7 +14,7 @@ const variables = css`
     --white: #e6f1ff;
     --green: #64ffda;
     --green-tint: rgba(100, 255, 218, 0.1);
-    --pink: #f57dff;
+    --pink: #b64d6d;
     --blue: #57cbff;
     --dark-purple: rgb(30 21 47);
     --white: #ffffff;
