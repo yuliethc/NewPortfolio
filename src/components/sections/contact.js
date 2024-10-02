fb3859498331492b1e359c0b840aa99c1e8703dc
Yulieth Lubo <yuliethdotcom@gmail.com>
2024-10-02 12:19:30 +0200

    contact

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -55,17 +55,17 @@ const Contact = () => {
 
   return (
     <StyledContactSection id="contact" ref={revealContainer}>
-      <h2 className="numbered-heading overline">What’s Next?</h2>
+      <h2 className="numbered-heading overline">Want to know more?</h2>
 
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
+        I'm always ready for new challenges and opportunities. My inbox is always open.
         Whether you have a question or just want to say hi, I’ll try my best to get back to you!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
-        Say Hello
+        Send an Email
       </a>
     </StyledContactSection>
   );
